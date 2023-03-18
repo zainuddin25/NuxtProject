3825389c8fadcc33c503a5a7caca9f541e08bfca
zainuddin25 <zanuk12345>
2023-03-18 20:27:49 +0700

    feat: news

---
+++ pages/news.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="pt-28">
+        <div class="container mx-auto px-4 lg:px-0">
+            <div class="flex gap-10 cursor-pointer mb-10 flex-col lg:flex-row">
+                <div class="flex flex-col lg:w-2/3 w-full">
+                    <span class="lg:text-2xl text-xl" id="bold">News Updated From Twitter</span>
+                    <div class="w-full h-1 bg-black my-6"></div>
+                    <span class="text-3xl lg:text-5xl" id="bold">Lorem ipsum dolor sit amet <span class="text-[#f34539]">consectetur adipisicing</span> elit. Sunt provident</span>
+                    <span class="my-6 text-black/50 text-sm lg:text-base" id="bold">Marth, 20 2023</span>
+                    <span class="text-sm text-black/50 lg:text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas fugit a, unde iusto accusamus quam aperiam repellendus repudiandae quia ad, rerum odio temporibus quaerat dicta nesciunt delectus omnis corrupti dolorum.
+                    Veritatis rem tempora ex voluptates ducimus voluptas delectus eveniet repudiandae quae quod. Ea provident temporibus minus maiores placeat corrupti ipsum. Eaque ad similique esse architecto unde, deleniti libero dolorem commodi!</span>
+                </div>
+                <div class="flex justify-center items-center w-full lg:w-1/3">
+                    <img src="@/assets/images/image-1.jpg" class="rounded-md" />
+                </div>
+            </div>
+            <span class="text-2xl lg:text-4xl" id="bold">This Week Update News</span>
+            <div class="w-full" v-for="(data, index) in news" :key="index">
+                <div class="w-full h-1 bg-black mt-6"></div>
+                <div class="flex gap-10 cursor-pointer mt-10 items-center flex-col lg:flex-row">
+                    <div class="flex justify-center items-center w-full lg:w-1/3">
+                        <img src="@/assets/images/image-4.jpg" />
+                    </div>
+                    <div class="flex flex-col w-full lg:w-1/3">
+                        <span class="text-4xl" id="bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt provident</span>
+                        <span class="mt-4 text-black/50" id="bold">Marth, 20 2023</span>
+                    </div>
+                    <div class="w-full lg:w-1/3">
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas fugit a, unde iusto accusamus quam aperiam repellendus repudiandae quia ad, rerum odio temporibus quaerat dicta nesciunt delectus omnis corrupti dolorum.
+                        Veritatis rem tempora ex voluptates ducimus voluptas delectus eveniet repudiandae quae quod.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const news = [1, 2, 3, 4, 5]
+</script>
--- pages/work.vue
+++ pages/work.vue
@@ -1,5 +1,40 @@
 <template>
-    <div class="mt-20">
-        <h1>Hallo</h1>
+    <div class="pt-28">
+        <div class="container mx-auto">
+            <div class="w-fit relative mb-8">
+                <span class="text-4xl" id="bold">Our Last Project</span>
+                <div class="absolute -bottom-2 h-1.5 w-[70%] bg-[#f34539]"></div>
+            </div>
+            <span class="text-sm text-black/50 w-2/4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae mollitia magni officia? Minima beatae sed molestias saepe dicta nemo laudantium delectus aut, illo rem sunt vel? Possimus quae impedit autem. Nihil porro qui ea ipsam inventore debitis illum nulla fuga quasi. Molestiae dolorum commodi cumque voluptatibus rerum natus voluptates dolores esse eligendi repellendus nisi sequi, eum accusamus veniam, obcaecati saepe.</span>
+            <div class="w-full bg-black h-1 mt-8"></div>
+            <div class="flex mt-8 gap-10 flex-col lg:flex-row">
+                <div class="w-full lg:w-3/4 flex flex-col">
+                    <span class="text-2xl text-center lg:text-start" id="bold">Latest Project</span>
+                    <span class="text-sm mt-4 text-black/50 text-center lg:text-start">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem facilis, velit error repellendus ea nihil corrupti quo quia dolore voluptates autem placeat sequi hic. Atque cumque exercitationem debitis corrupti odit.
+                        Commodi consectetur vitae ipsum quisquam corporis labore cupiditate fuga in ab consequatur placeat mollitia harum quis neque inventore non amet natus rerum quaerat similique rem hic, reprehenderit laboriosam.
+                    </span>
+                    <div class="w-full grid grid-cols-2 lg:grid-cols-3 mt-6 gap-8">
+                        <img src="@/assets/images/2d-modelling.svg" class="rounded-md" />
+                        <img src="@/assets/images/2d-modelling.svg" class="rounded-md" />
+                        <img src="@/assets/images/2d-modelling.svg" class="rounded-md" />
+                        <img src="@/assets/images/2d-modelling.svg" class="rounded-md" />
+                        <img src="@/assets/images/2d-modelling.svg" class="rounded-md" />
+                        <img src="@/assets/images/2d-modelling.svg" class="rounded-md" />
+                    </div>
+                </div>
+                <div class="w-full lg:w-1/4">
+                    <div class="static lg:sticky top-28 right-0">
+                        <span class="text-4xl" id="bold">Lorem ipsum dolor sit amet consectetur</span>
+                        <button class="mt-4 text-sm px-4 py-2 lg:px-8 lg:py-3 border-2 rounded-md text-[#f34539] border-[#f34539] hover:bg-[#f34539] hover:text-white duration-200">
+                            Order Now
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+</script>
