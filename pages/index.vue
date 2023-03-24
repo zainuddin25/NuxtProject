1c5e8151075e868373e0ce97ac97514932cd7c4f
zainuddin25 <zanuk12345>
2023-03-24 11:07:12 +0700

    feat: video not found in service

--- pages/index.vue
+++ pages/index.vue
@@ -58,7 +58,7 @@
                     </div>
                 </div>
             </div>
-            <div class="gap-6 mt-8 lg:mt-0 hidden lg:block">
+            <div class="gap-6 mt-8 lg:mt-0">
                 <div class="flex items-center gap-4">
                     <div class="w-fit relative">
                         <video autoplay muted loop class="border-0 lg:border-[16px] border-white lg:rounded-3xl shadow-2xl lg:w-[300px] w-full rounded-none">
